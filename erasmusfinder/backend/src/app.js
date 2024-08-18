e3ca34e527738ee4936cddd6b0af409f54d239ff
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-18 12:25:49 +0200

    Intentando solucionar fallo backend

--- erasmusfinder/backend/src/app.js
+++ erasmusfinder/backend/src/app.js
@@ -8,13 +8,7 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 
 //Middlewares
-app.use(cors(
-    {
-        origin: [""],
-        methods: ["POST","GET"],
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
