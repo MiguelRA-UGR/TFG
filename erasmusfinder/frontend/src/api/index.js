fb1ca39cbcac13b2f804ef18ca819e1f8e10ca64
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-18 12:16:50 +0200

    Intentando solucionar fallo backend

--- erasmusfinder/frontend/src/api/index.js
+++ erasmusfinder/frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://tfg-miguelra-ugrs-projects.vercel.app/' });
+const API = axios.create({ baseURL: 'http://localhost:4000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
