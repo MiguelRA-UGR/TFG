86a9a39d31f62d6c65f0c191856a3151aaf13807
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-19 12:14:15 +0200

    Modificada configuracion de express para imagenes

--- erasmusfinder/backend/src/index.js
+++ erasmusfinder/backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cors(
     }
 ));
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/imgs', express.static(path.join(__dirname, 'public/imgs')));
 
 // Rutas existentes
 app.get('/', (req, res) => {
