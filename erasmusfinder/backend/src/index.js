9ef04cf52885e7a630eaa4baa39783c4f0f91a18
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-19 12:18:54 +0200

    Modificada configuracion de express para imagenes

--- erasmusfinder/backend/src/index.js
+++ erasmusfinder/backend/src/index.js
@@ -14,7 +14,7 @@ app.set('port', process.env.PORT || 4000);
 // Middlewares
 app.use(cors(
     {
-        origin: ['http://localhost:3000'],
+        origin: [''],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
