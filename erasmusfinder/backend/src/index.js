4896aa68c905d73fe5dc2c51e90059cee28fa90f
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-27 12:44:19 +0200

    Modificado cors para cualquier origen

--- erasmusfinder/backend/src/index.js
+++ erasmusfinder/backend/src/index.js
@@ -13,12 +13,11 @@ app.set('port', process.env.PORT || 4000);
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
 app.use(cors(
-//     {
-//     origin: allowedOrigins,
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//     credentials: true
-// }
-));
+    {
+    origin: '*',
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 
 
 app.use(express.json());
