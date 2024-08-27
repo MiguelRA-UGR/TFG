a4cf0528aa7dc0ff277df1312f605a39a6faea91
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-27 13:01:02 +0200

    Prueba con app options

--- erasmusfinder/backend/src/index.js
+++ erasmusfinder/backend/src/index.js
@@ -15,9 +15,11 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 app.use(cors(
     {
     origin: allowedOrigins,
-    methods: ["POST", "GET", "PUT", "DELETE"]
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
 }));
 
+app.options("*", cors())
 
 app.use(express.json());
 app.use(express.static('public'));
