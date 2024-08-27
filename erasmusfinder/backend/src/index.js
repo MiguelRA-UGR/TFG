5f57c49a9c3db43badd26e16f4b37411e3bc2f8a
MiguelRA-UGR <e.miguelra@go.ugr.es>
2024-08-27 12:34:59 +0200

    Añadidos orígenes como variables de entorno cambiados

--- erasmusfinder/backend/src/index.js
+++ erasmusfinder/backend/src/index.js
@@ -12,11 +12,13 @@ app.set('port', process.env.PORT || 4000);
 // Middlewares
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
-app.use(cors({
+app.use(cors(
+    {
     origin: allowedOrigins,
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
-}));
+}
+));
 
 
 app.use(express.json());
